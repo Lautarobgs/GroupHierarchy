5e7e5667dd5da28e4a585f4bdfb91b3dc6f9d75a
lucas-software <lucas.lr.2516@gmail.com>
2024-10-31 21:10:20 -0300

    Patterns MVC,Strategy and Proxy

--- src/main/java/org/example/service/SmartphoneService.java
+++ src/main/java/org/example/service/SmartphoneService.java
@@ -1,7 +1,6 @@
 package org.example.service;
 
 import org.example.dao.GenericDao;
-import org.example.model.Device;
 import org.example.model.Smartphone;
 import java.sql.SQLException;
 import org.example.dao.SmartphoneDAO;
@@ -10,7 +9,7 @@ import java.util.List;
 public class SmartphoneService implements IGenericService<Smartphone,Integer>
 {
     private final GenericDao<Smartphone,Integer> smartphoneDAO = new SmartphoneDAO();
-
+    @Override
     public void add(Smartphone Smartphone) throws SQLException {
         smartphoneDAO.insert(Smartphone);
     }
